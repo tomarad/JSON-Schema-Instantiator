92bc2eb61bce5181344749b459a2d080d3795427
Roman Gotsiy <gotsijroman@gmail.com>
2015-10-31 16:33:01 +0200

    Fix tests code style

--- tests/tests.js
+++ tests/tests.js
@@ -1,51 +1,51 @@
-var expect = require("chai").expect;
+var expect = require('chai').expect;
 
 var instantiate = require('../src/instantiator').instantiate;
 
 var schema, expected, result;
 
-describe("Primitives", function(){
+describe('Primitives', function() {
 
-  it("should instantiate string", function(){
+  it('should instantiate string', function() {
     schema = {
-      "type": "string"
+      'type': 'string'
     };
     result = instantiate(schema);
     expected = "";
     expect(result).to.deep.equal(expected);
   });
 
-  it("should instantiate number", function(){
+  it('should instantiate number', function() {
     schema = {
-      "type": "number"
+      'type': 'number'
     };
     result = instantiate(schema);
     expected = 0;
     expect(result).to.deep.equal(expected);
   });
 
-  it("should instantiate boolean", function(){
+  it('should instantiate boolean', function() {
     schema = {
-      "type": "boolean"
+      'type': 'boolean'
     };
     result = instantiate(schema);
     expected = false;
     expect(result).to.deep.equal(expected);
   });
 
-  it("should instantiate null", function(){
+  it('should instantiate null', function() {
     schema = {
-      "type": "null"
+      'type': 'null'
     };
     result = instantiate(schema);
     expected = null;
     expect(result).to.deep.equal(expected);
   });
 
-  it("should use default property", function(){
+  it('should use default property', function() {
     schema = {
-      "type": "number",
-      "default": 100
+      'type': 'number',
+      'default': 100
     };
     result = instantiate(schema);
     expected = 100;
@@ -54,90 +54,90 @@ describe("Primitives", function(){
 });
 
 
-describe("Objects", function(){
-  it("should instantiate object without properties", function(){
+describe('Objects', function() {
+  it('should instantiate object without properties', function() {
     schema = {
-      "type": "object"
+      'type': 'object'
     };
     result = instantiate(schema);
     expected = {};
     expect(result).to.deep.equal(expected);
   });
 
-  it("should instantiate object with property", function(){
+  it('should instantiate object with property', function() {
     schema = {
-      "type": "object",
-      "properties": {
-        "title": {
-          "type": "string",
+      'type': 'object',
+      'properties': {
+        'title': {
+          'type': 'string',
         }
       }
     };
     result = instantiate(schema);
     expected = {
-      "title": ""
+      'title': ''
     };
     expect(result).to.deep.equal(expected);
   });
 
-  it("should instantiate object with property with default value", function(){
+  it('should instantiate object with property with default value', function() {
     schema = {
-      "type": "object",
-      "properties": {
-        "title": {
-          "type": "string",
-          "default": "Example"
+      'type': 'object',
+      'properties': {
+        'title': {
+          'type': 'string',
+          'default': 'Example'
         }
       }
     };
     result = instantiate(schema);
     expected = {
-      "title": "Example"
+      'title': 'Example'
     };
     expect(result).to.deep.equal(expected);
   });
 
-  it("should instantiate object with more than one property", function(){
+  it('should instantiate object with more than one property', function() {
     schema = {
-      "type": "object",
-      "properties": {
-        "title": {
-          "type": "string",
-          "default": "Example"
+      'type': 'object',
+      'properties': {
+        'title': {
+          'type': 'string',
+          'default': 'Example'
         },
-        "amount": {
-          "type": "number",
-          "default": 10
+        'amount': {
+          'type': 'number',
+          'default': 10
         }
       }
     };
     result = instantiate(schema);
     expected = {
-      "title": "Example",
-      "amount": 10
+      'title': 'Example',
+      'amount': 10
     };
     expect(result).to.deep.equal(expected);
   });
 });
 
-describe("AllOf", function(){
-  it("should instantiate schema with allOf", function(){
+describe('AllOf', function() {
+  it('should instantiate schema with allOf', function() {
     schema = {
-      "allOf": [
+      'allOf': [
         {
-          "type": "object",
-          "properties": {
-            "title": {
-              "type": "string"
+          'type': 'object',
+          'properties': {
+            'title': {
+              'type': 'string'
             }
           }
         },
         {
-          "type": "object",
-          "properties": {
-            "amount": {
-              "type": "number",
-              "default": 1
+          'type': 'object',
+          'properties': {
+            'amount': {
+              'type': 'number',
+              'default': 1
             }
           }
         }
@@ -145,8 +145,8 @@ describe("AllOf", function(){
     };
     result = instantiate(schema);
     expected = {
-      "title": "",
-      "amount": 1
+      'title': '',
+      'amount': 1
     };
     expect(result).to.deep.equal(expected);
   });
